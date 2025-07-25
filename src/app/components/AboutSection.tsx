af19fb09ddd5087dd9061cdb41357921ee2cfd4c
MichaelJOHare <mjohare13@gmail.com>
2025-07-24 21:04:13 -0400

    fix build error

--- src/app/components/AboutSection.tsx
+++ src/app/components/AboutSection.tsx
@@ -11,14 +11,14 @@ export const AboutSection = () => {
           <div className="grid grid-cols-1 items-center gap-12 md:grid-cols-2 lg:gap-16">
             <div>
               <h2 className="text-3xl leading-tight font-bold text-black sm:text-4xl lg:text-5xl dark:text-white">
-                Hi, I'm Michael O'Hare
+                Hi, I&apos;m Michael O&apos;Hare
               </h2>
               <p className="mt-6 text-lg leading-relaxed text-gray-700 dark:text-gray-300">
-                Welcome to my website—it's still a work in progress, so new
-                things will keep being added. I'm Michael or Mike, whichever you
-                prefer. I love spending time outdoors doing things like
-                backpacking, skiing, and fishing. Having lived in Colorado for
-                10 years, I was able to enjoy all of those quite a lot.
+                Welcome to my website&mdash;it&apos;s still a work in progress,
+                so new things will keep being added. I&apos;m Michael or Mike,
+                whichever you prefer. I love spending time outdoors doing things
+                like backpacking, skiing, and fishing. Having lived in Colorado
+                for 10 years, I was able to enjoy all of those quite a lot.
               </p>
             </div>
 
@@ -46,12 +46,12 @@ export const AboutSection = () => {
               </h2>
               <p className="mt-6 text-lg leading-relaxed text-gray-700 dark:text-gray-300">
                 I like learning new coding languages by developing something
-                chess-related. If you can't tell by my projects page (upper
-                right corner), I really like chess. Plus it's very fun to build
-                a chess app from scratch—there's always something to improve.
-                Whether it's adding the Stockfish chess engine or implementing a
-                way to import FEN strings to the board, you'll always have
-                something to work on.
+                chess-related. If you can&apos;t tell by my projects page (upper
+                right corner), I really like chess. Plus it&apos;s very fun to
+                build a chess app from scratch&mdash;there&apos;s always
+                something to improve. Whether it&apos;s adding the Stockfish
+                chess engine or implementing a way to import FEN strings to the
+                board, you&apos;ll always have something to work on.
               </p>
             </div>
             <div className="relative flex justify-center md:order-first">
@@ -74,15 +74,15 @@ export const AboutSection = () => {
           <div className="grid grid-cols-1 items-center gap-12 md:grid-cols-2 lg:gap-16">
             <div>
               <h2 className="text-3xl leading-tight font-bold text-black sm:text-4xl lg:text-5xl dark:text-white">
-                Where I'm at and where I'm going
+                Where I&apos;m at and where I&apos;m going
               </h2>
               <div className="mt-6 space-y-4 text-lg leading-relaxed text-gray-700 dark:text-gray-300">
                 <p>
                   I recently started a new job working in support for OpenVMS
-                  (one of the oldest operating systems!) so I haven't had a lot
-                  of time to focus on side projects. But I've been doing some
-                  learning in my free time with new web frameworks. It's an
-                  exciting time to be working in tech!
+                  (one of the oldest operating systems!) so I haven&apos;t had a
+                  lot of time to focus on side projects. But I&apos;ve been
+                  doing some learning in my free time with new web frameworks.
+                  It&apos;s an exciting time to be working in tech!
                 </p>
                 <p>
                   If you like anything you see here and want to team up for some
